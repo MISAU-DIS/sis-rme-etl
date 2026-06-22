7b88662d4d712b4c365d0ffd584c69f063007258
JPboane <jpboane@gmail.com>
2026-06-22 17:59:56 +0200

    Fix issue

--- api/src/main/java/org/openmrs/module/epts/etl/etl/processor/transformer/SimpleValueTransformer.java
+++ api/src/main/java/org/openmrs/module/epts/etl/etl/processor/transformer/SimpleValueTransformer.java
@@ -62,9 +62,10 @@ public class SimpleValueTransformer extends AbstractEtlFieldTransformer {
 			EtlDatabaseObject transformedRecord, List<EtlDatabaseObject> additionalSrcObjects, TransformableField field,
 			Connection srcConn, Connection dstConn) throws DBException, EtlTransformationException {
 
-		Object result = EtlFieldTransformer.tryToReplaceParametersOnSrcValue(
-				field.getTransformationTargetObject().getRelatedEtlConf(), additionalSrcObjects,
-				field.getValueToTransform());
+		Object result = EtlFieldTransformer
+				.tryToReplaceParametersOnSrcValue(field.getTransformationTargetObject() != null
+						? field.getTransformationTargetObject().getRelatedEtlConf()
+						: null, additionalSrcObjects, field.getValueToTransform());
 
 		FieldTransformingInfo transformingInfo = new FieldTransformingInfo(field, result, null);
 
